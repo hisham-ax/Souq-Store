af751f716620b2bf3a2532639866e0655c31491b
hisham <hisham4a4d4m@gmail.com>
2026-03-14 23:18:14 +0200

    convert database from local to atlas

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 //connected to database
 mongoose
-  .connect(process.env.MONGO_DATABASE_URI)
+  .connect(process.env.MONGODB_URL_ATLAS)
   .then(() => {
     console.log("connected to database");
   })
