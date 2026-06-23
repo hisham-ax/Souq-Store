b67b8069543a7a0c59a4efd6a4c0009dcbb0957b
hisham <hisham4a4d4m@gmail.com>
2026-06-23 21:26:50 +0300

    update the name of ordermodel

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
-import Order from "../models/orderModel.js";
+import Order from "../models/OrderModel.js";
 import { isAuth } from "../utils.js";
 const orderRouter = express.Router();
 
