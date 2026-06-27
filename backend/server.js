d79f7346839829f7d2af38ff2e4d9db71363a6e0
hisham <hisham4a4d4m@gmail.com>
2026-06-27 20:33:28 +0300

    disable app listen

--- backend/server.js
+++ backend/server.js
@@ -42,8 +42,8 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`server at http://localhost :${port}`);
-});
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`server at http://localhost :${port}`);
+// });
 export default app;
